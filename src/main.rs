7e9d53c94adaa2f4a7dd57432f2e0a6787a523a3
Bart Massey <bart.massey@gmail.com>
2025-12-03 20:09:38 -0800

    got texture view example running

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,32 @@
 use nannou::prelude::*;
 
-struct Model {}
+struct Model {
+    texture: wgpu::Texture,
+}
 
-fn model(_app: &App) -> Model {
-    Model {}
+fn model(app: &App) -> Model {
+  app.new_window().size(512, 512).view(view).build().unwrap();
+  let assets = app.assets_path().unwrap();
+  let img_path = assets.join("nature_1.jpg");
+  let texture = wgpu::Texture::from_path(app, img_path).unwrap();
+  Model { texture }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
-fn view(app: &App, _model: &Model, frame: Frame){
-    let win = app.window_rect();
-    let x_y = win.wh() / 8.0;
-    let w_h = win.wh() / 2.0;
+fn view(app: &App, model: &Model, frame: Frame){
+    frame.clear(BLACK);
+
     let draw = app.draw();
-    draw.background().color(PURPLE);
-    draw.rect()
-        .x_y(x_y.x, x_y.y)
-        .w_h(w_h.x, w_h.y)
-        .z_degrees(app.time * 50.0)
-        .color(WHITE);
+    draw.texture(&model.texture);
+
     draw.to_frame(app, &frame).unwrap();
 }
 
 fn main() {
     nannou::app(model)
-        .update(update)
-        .simple_window(view)
+//        .update(update)
+//        .simple_window(view)
         .run();
 }
