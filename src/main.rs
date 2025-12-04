b7d0858a47624007b4d68581e94ba2d28a6728c9
Bart Massey <bart.massey@gmail.com>
2025-12-03 21:20:24 -0800

    finally achieved mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 use nannou::prelude::*;
 use nannou::image::{DynamicImage, ImageBuffer, Rgba};
+use num::Complex;
 
 struct Model {
     texture: wgpu::Texture,
 }
+
+// Thanks to Jim Blandy for the initial implementation of this function.
+fn mandel(x: f32, y: f32) -> u8 {
+    let c = Complex::new(x, y);
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..255 {
+        if z.norm_sqr() > 4.0 {
+            return i
+        }
+        z = z * z + c;
+    }
+
+    255
+}
+
 
 fn model(app: &App) -> Model {
     let window = app.main_window();
     let (w, h) = window.rect().w_h();
 
     let img = ImageBuffer::from_fn(w.floor() as u32, h.floor() as u32, |x, y| {
-        Rgba([
-            ((x * y) % 256) as u8,
-            (x % 256) as u8,
-            (y % 256) as u8,
-            255,
-        ])
+        let m = mandel(x as f32 / w - 0.2, y as f32 / h);
+        Rgba([m, m, m, 255])
     });
 
     let dynamic_img = DynamicImage::ImageRgba8(img);
@@ -35,9 +48,11 @@ fn view(app: &App, model: &Model, frame: Frame) {
         (point, tex_coords)
     });
 
-    draw.scale(100.0 * app.time)
+    let t = app.time;
+    draw.scale(100.0 * t)
         .polygon()
-        .points_textured(&model.texture, points);
+        .points_textured(&model.texture, points)
+        .rotate(t);
     draw.to_frame(app, &frame).unwrap();
 }
 
