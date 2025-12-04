add222ffc6ac9d467be68c229588c6824e6b4b53
Bart Massey <bart.massey@gmail.com>
2025-12-03 20:35:04 -0800

    got app to render texture as points in polygon

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,35 @@ struct Model {
 }
 
 fn model(app: &App) -> Model {
-  app.new_window().size(512, 512).view(view).build().unwrap();
-  let assets = app.assets_path().unwrap();
-  let img_path = assets.join("nature_1.jpg");
-  let texture = wgpu::Texture::from_path(app, img_path).unwrap();
-  Model { texture }
+    let assets = app.assets_path().unwrap();
+    let img_path = assets.join("nature_1.jpg");
+    let texture = wgpu::Texture::from_path(app, img_path).unwrap();
+    Model { texture }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
-}
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(app: &App, model: &Model, frame: Frame){
+fn view(app: &App, model: &Model, frame: Frame) {
     frame.clear(BLACK);
 
     let draw = app.draw();
-    draw.texture(&model.texture);
+    let rect = geom::Rect::from_wh(vec2(1.0, 1.0));
+    let ellipse = geom::Ellipse::new(rect, 100.0).circumference();
+    let points = ellipse.map(|p| {
+        let point = Point2::from(p);
+        let tex_coords = [point.x + 0.5, 1.0 - (point.y + 0.5)];
+        (point, tex_coords)
+    });
 
+    draw.scale(100.0 * app.time)
+        .polygon()
+        .points_textured(&model.texture, points);
     draw.to_frame(app, &frame).unwrap();
 }
 
 fn main() {
     nannou::app(model)
-//        .update(update)
-//        .simple_window(view)
+        //        .update(update)
+        .simple_window(view)
         .run();
 }
